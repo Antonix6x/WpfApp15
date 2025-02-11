fafffd5575664877ba1973b0ce2c1151c651b54c
Antoni Wittstock <antoni.wittstock@icloud.com>
2025-02-11 14:06:53 +0100

    lol

--- WpfApp15/MainWindow.xaml.cs
+++ WpfApp15/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,18 +16,47 @@ namespace WpfApp15
     /// </summary>
     public partial class MainWindow : Window
     {
-        string maleLitery= "qwertyuiopasdfghjklzxcvbnm";
-        string znakiSpecjalne = "!@#$%^&*()_+";
-        string Wielkielitery = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        private string wygenerowaneHaslo = "Hasło nie zostało wygenerowane";
+
+        
+
         public MainWindow()
         {
-            InitializeComponent();
         }
 
-        private void generujHaslo_Click(object sender, RoutedEventArgs e)
-        {
-           Random random = new Random();
+        private string GenerujHaslo(int dlugosc, bool czyWielkieLitery, bool czyCyfry, bool czySpecjalne)
+            {
+            string maleLitery = "qwertyuiopasdfghjklzxcvbnm";
+            string znakiSpecjalne = "!@#$%^&*()_+";
+            string Wielkielitery = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string cyfry = "0123456789";
 
+            Random random = new Random();
+            char[] haslo = new char[dlugosc];
+            for (int i = 0; i < dlugosc; i++)
+            {
+                haslo[i] = maleLitery[random.Next(maleLitery.Length)];
+
+            }
+            if (czyWielkieLitery && dlugosc > 1)
+            {
+                haslo[0] = Wielkielitery[random.Next(Wielkielitery.Length)];
+            }
+            if (czyCyfry && dlugosc > 2)
+            {
+                haslo[1] = cyfry(random.Next(cyfry.Length));
+            }
+            if (czySpecjalne && dlugosc > 3)
+            {
+                haslo[dlugosc - 1] = znakiSpecjalne[random.Next(znakiSpecjalne.Length)];
+            }
+            return new string(haslo);
+
+        }
+
+        private void generujHaslo_Click(int dlugosc, bool czyWielkieLitery, bool czyCyfry, bool czySpecjalne)
+        {
+           
         }
     }
 }
